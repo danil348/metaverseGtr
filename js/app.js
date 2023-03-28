dd1f27721aaeb351ee0c88bc3cc916e96db68a71
danil348 <eliseevdanil348@gmail.com>
2023-03-28 13:32:04 +0300

    asd

--- js/app.js
+++ js/app.js
@@ -5,6 +5,29 @@ window.onload = function () {
 	_icon_menu()
 	_smoothscroll()
 	//========================================================================================================================================================
+	let index = 0,
+    interval = 2700;
+
+	const rand = (min, max) => 
+		Math.floor(Math.random() * (max - min + 1)) + min;
+
+	const animate = star => {
+		star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+		star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+		star.style.animation = "none";
+		star.offsetHeight;
+		star.style.animation = "";
+	}
+
+	for(const star of document.getElementsByClassName("mainScreen-image__star")) {
+		setTimeout(() => {
+			animate(star);
+			
+			setInterval(() => animate(star), 2700);
+		}, index++ * (interval / 4))
+	}
+	//========================================================================================================================================================
 	const blob = document.getElementById("blob");
 	let clientX;
 	let clientY;
